e2a7d6dc69c1692a80b17afb01ef656d3ef3a514
JessieChan0730 <3099570972@qq.com>
2024-07-29 21:13:13 +0800

    fix：修改用户信息返回的数据结构

--- blog_api/apps/user/views.py
+++ blog_api/apps/user/views.py
@@ -2,19 +2,19 @@ from django.contrib.auth.models import User
 from rest_framework import status
 from rest_framework.decorators import action
 from rest_framework.mixins import UpdateModelMixin
-from rest_framework.permissions import IsAuthenticatedOrReadOnly
 from rest_framework.request import Request
 from rest_framework.response import Response
 from rest_framework.viewsets import GenericViewSet
 from rest_framework_simplejwt.authentication import JWTAuthentication
-
 from user.models import UserDetail
 from user.serializer import UserDetailSerializer, ChangePasswordSerializer
+
+from blog_api.utils.result_data import ResultData
 
 
 class UserDetailViewSet(GenericViewSet, UpdateModelMixin):
     authentication_classes = [JWTAuthentication]  # 认证方式
-    permission_classes = [IsAuthenticatedOrReadOnly]  # 权限类，匿名用户只读，登录用户可以操作
+    # permission_classes = [IsAuthenticatedOrReadOnly]  # 权限类，匿名用户只读，登录用户可以操作
     queryset = UserDetail.objects.all()
 
     # 访问action时，使用ChangePasswordSerializer序列化器
@@ -31,11 +31,9 @@ class UserDetailViewSet(GenericViewSet, UpdateModelMixin):
         user = UserDetail.objects.first()
         if user is not None:
             serializer = self.get_serializer(user)
-            return Response(data=serializer.data, status=status.HTTP_200_OK)
+            return Response(data=ResultData.ok_200(data=serializer.data), status=status.HTTP_200_OK)
         else:
-            return Response(data={
-                'message': '用户不存在',
-            }, status=status.HTTP_404_NOT_FOUND)
+            return Response(data=ResultData.bad_request_400(msg="该用户不存在"), status=status.HTTP_404_NOT_FOUND)
 
         # 修改密码
 

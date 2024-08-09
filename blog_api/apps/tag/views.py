1b62f52d14ca513998d5198c56e3550bf4a394a5
JessieChan0730 <3099570972@qq.com>
2024-08-09 22:08:46 +0800

    fix: 添加tag接口分页功能

--- blog_api/apps/tag/views.py
+++ blog_api/apps/tag/views.py
@@ -1,9 +1,15 @@
 from rest_framework import viewsets
-
+from rest_framework.permissions import IsAuthenticatedOrReadOnly
+from rest_framework_simplejwt.authentication import JWTAuthentication
 from tag.models import Tag
 from tag.serializer import TagSerializer
+
+from .pagination import TagPagination
 
 
 class TagViewSet(viewsets.ModelViewSet):
     queryset = Tag.objects.all()
     serializer_class = TagSerializer
+    authentication_classes = [JWTAuthentication]  # 认证方式
+    permission_classes = [IsAuthenticatedOrReadOnly]
+    pagination_class = TagPagination

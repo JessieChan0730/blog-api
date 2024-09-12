d740e9cf5d07b91fc7e77c392bb394412a79e67b
JessieChan0730 <3099570972@qq.com>
2024-09-12 16:37:53 +0800

    feat: 标签添加color字段

--- blog_api/apps/tag/models.py
+++ blog_api/apps/tag/models.py
@@ -4,6 +4,7 @@ from django.db import models
 # Create your models here.
 class Tag(models.Model):
     name = models.CharField(max_length=10)
+    color = models.CharField(max_length=8,default='#409eff')
 
     class Meta:
         db_table = 'tags'
--- blog_api/apps/tag/serializer.py
+++ blog_api/apps/tag/serializer.py
@@ -6,14 +6,17 @@ from .models import Tag
 class TagSerializer(serializers.Serializer):
     id = serializers.IntegerField(read_only=True)
     name = serializers.CharField(max_length=10, required=True)
+    color = serializers.CharField(max_length=8, required=False, default='#409EFF')
 
     def create(self, validated_data):
         name = validated_data.get("name")
-        tag = Tag.objects.create(name=name)
+        color = validated_data.get("color")
+        tag = Tag.objects.create(name=name, color=color)
         return tag
 
     def update(self, instance, validated_data):
         instance.name = validated_data.get("name", instance.name)
+        instance.color = validated_data.get("color", instance.color)
         instance.save()
         return instance
 
